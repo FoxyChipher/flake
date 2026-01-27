f4009f5a641a41e5ec7deee866d29af409c845b2
FoxyChipher <ageev-eldar@mail.ru>
2026-01-27 18:18:03 +0300

    greetd fix

--- configuration.nix
+++ configuration.nix
@@ -103,6 +103,8 @@
 	
 	# ========== SERVICES ==========
 	services = {
+		gnome.gnome-keyring.enable = lib.mkForce false;
+		desktopManager.gnome.enable = lib.mkForce false;
 		pipewire = {
 			enable = true;
 			alsa.enable = true;
@@ -112,22 +114,24 @@
 			wireplumber.enable = true;
 		};
 		xserver = {
+			enable = true;
 			videoDrivers = ["nvidia"];
 			xkb.layout = "us,ru";
 			xkb.options = "grp:lalt_lshift_toggle;";
 		};
 		greetd = {
 			enable = true;
+			useTextGreeter = true;
 			settings = {
 				default_session = {
-					command = "${pkgs.tuigreet}/bin/tuigreet --time --remember";
+					command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --remember-session --xsessions ${config.services.displayManager.sessionData.desktops}/share/xsessions --sessions ${config.services.displayManager.sessionData.desktops}/share/wayland-sessions";
 					user = "greeter";
 				};
 			};
 		};
 	};
-
-	systemd.user.services.niri-flake-polkit.enable = false;
+	
+	# systemd.user.services.niri-flake-polkit.enable = false;
 	# ========== FONTS ==========
 	fonts.packages = with pkgs; [
 		nerd-fonts.fira-code
@@ -137,16 +141,26 @@
 	];
 	
 	# ========== XDG PORTAL ==========
-	xdg.portal = {
-		enable = true;
-		extraPortals = with pkgs; [
-			xdg-desktop-portal-wlr
-			xdg-desktop-portal-gtk
-			xdg-desktop-portal-termfilechooser
-		];
-		config.common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];				
+	xdg = {
+		mime.enable = true;
+		mime.defaultApplications = { "inode/directory" = "yazi.desktop"; };
+		portal = {
+			enable = true;
+			xdgOpenUsePortal = true;
+			config = {
+				common."org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ]; # IMPORTANT!
+				common."org.freedesktop.impl.portal.ScreenCast" = "wlr";
+				common."org.freedesktop.impl.portal.Screenshot" = "wlr";
+				common.default = [ "*" ]; 
+			};
+			extraPortals = lib.mkForce (with pkgs;  [
+				xdg-desktop-portal-wlr
+   				xdg-desktop-portal-gtk
+   				xdg-desktop-portal-termfilechooser
+			]);				
+		};
 	};
-	
+
 	# ========== PROGRAMS ==========
 	programs = {
 		fish.enable = true;
@@ -207,6 +221,7 @@
 		git
 		micro-full
 		sublime4
+		vscodium
 		fzf
 		wget
 		btop
@@ -259,146 +274,147 @@
 		obs-studio-plugins.obs-vaapi
 		nvidia-vaapi-driver
 		cmd-polkit
+		tuigreet
+		niri-unstable
 	];
 
 	
 	# ========== ENVIRONMENT VARS ==========
 	environment.sessionVariables = lib.mkForce {
-		# Базовые Wayland настройки
-		XDG_SESSION_TYPE = "wayland";
-		XDG_SESSION_DESKTOP = "niri";
-		XDG_CURRENT_DESKTOP = "niri";
-
-		# Терминал и редакторы
-		TERMINAL = "kitty";
-		EDITOR = "micro";
-		SUDO_EDITOR = "micro";
-		VISUAL = "subl";
-
-		# Kitty
-		KITTY_ENABLE_WAYLAND = "1";
-
-		# GTK/ATK
-		NO_AT_BRIDGE = "1";
-
-		# NVIDIA Wayland поддержка
-		GBM_BACKEND = "nvidia-drm";
-		__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-
-		# GDK/Clutter
-		GDK_BACKEND = "wayland,x11,*";
-		CLUTTER_BACKEND = "wayland";
-		CLUTTER_DEFAULT_FPS = "60";
-
-		# Qt
-		QT_QPA_PLATFORM = "wayland;xcb";
-		QT_QPA_PLATFORMTHEME = "qt6ct";
-		QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
-
-		# SDL
-		SDL_VIDEODRIVER = "wayland,x11,windows";
-
-		# Java
-		_JAVA_AWT_WM_NONREPARENTING = "1";
-
-		# Electron
-		ELECTRON_OZONE_PLATFORM_HINT = "wayland";
-
-		# NVIDIA кодеков
-		GST_PLUGIN_FEATURE_RANK = "nvmpegvideodec:MAX,nvmpeg2videodec:MAX,nvmpeg4videodec:MAX,nvh264sldec:MAX,nvh264dec:MAX,nvjpegdec:MAX,nvh265sldec:MAX,nvh265dec:MAX,nvvp9dec:MAX";
-		GST_VAAPI_ALL_DRIVERS = "1";
-
-		# VA-API/VDPAU
-		LIBVA_DRIVER_NAME = "nvidia";
-		VAAPI_MPEG4_ENABLED = "true";
-		VDPAU_DRIVER = "nvidia";
-
-		# Firefox
-		MOZ_DISABLE_RDD_SANDBOX = "1";
-		MOZ_ENABLE_WAYLAND = "1";
-		MOZ_X11_EGL = "1";
-
-		# NVIDIA Direct Rendering
-		NVD_BACKEND = "direct";
-
-		# OBS Studio
-		OBS_USE_EGL = "1";
-
-		# MangoHud
-		MANGOHUD = "1";
-		MANGOHUD_DLSYM = "1";
-
-		# Wine
-		# //WINEPREFIX = "$HOME/.wine";
-		# //WINEARCH = "win64";
-		STAGING_SHARED_MEMORY = "1";
-
-		# NVIDIA OpenGL оптимизации
-		__GL_SHADER_CACHE = "1";
-		__GL_SHADER_DISK_CACHE = "1";
-		__GL_SHADER_DISK_CACHE_SKIP_CLEANUP = "1";
-		__GL_ExperimentalPerfStrategy = "1";
-		__GL_ConformantBlitFramebufferScissor = "1";
-		__GL_MaxFramesAllowed = "1";
-		__GL_SYNC_TO_VBLANK = "0";
-		__GL_YIELD = "NOTHING";
-
-		# Ввод
-		GLFW_IM_MODULE = "none";
-
-		# Синхронизация/VSync
-		mesa_glthread = "true";
-		vblank_mode = "0";
-		gl_vsync = "0";
-		vsync = "1";  # Может конфликтовать с vblank_mode=0
-
-		# Vulkan
-		MESA_VK_WSI_PRESENT_MODE = "immediate";
-
-		# DXVK
-		DXVK_SHADER_OPTIMIZE = "1";
-		DXVK_ENABLE_NVAPI = "1";
-		DXVK_ASYNC = "1";
-		DXVK_FRAME_RATE = "60";
-		DXVK_CONFIG = "dxgi.syncInterval=0; d3d9.presentInterval=0";
-
-		# VkBasalt
-		ENABLE_VKBASALT = "0";
-
-		# Аудио
-		PIPEWIRE_LATENCY = "512/48000";
-		PULSE_LATENCY_MSEC = "60";
-
-		# Proton
-		PROTON_ENABLE_NGX_UPDATER = "1";
-		PROTON_ENABLE_NVAPI = "1";
-		PROTON_FORCE_LARGE_ADDRESS_AWARE = "1";
-		PROTON_HIDE_NVIDIA_GPU = "0";
-		PROTON_USE_NTSYNC = "1";
-		# //PROTON_ENABLE_WAYLAND = "1";
-		PROTON_LOG = "1";
-
-		# VKD3D
-		VKD3D_CONFIG = "dxr";
-
-		# Wayland/XWayland
-		vk_xwayland_wait_ready = "false";
-
-		# GTK настройки
-		GTK_USE_IEC_UNITS = "1";
-		GTK_OVERLAY_SCROLLING = "1";
-		GTK_USE_PORTAL = "1";
-		GDK_DEBUG = "portals";
-
-		# NixOS специфичные
-		NIXOS_OZONE_WL = "1";
-
-		# Telegram Desktop
-		# TDESKTOP_USE_GTK_FILE_DIALOG = "1";
-		# TDESKTOP_I_KNOW_ABOUT_GTK_INCOMPATIBILITY = "1";
+# 		# Базовые Wayland настройки
+# 		XDG_SESSION_TYPE = "wayland";
+# 		XDG_SESSION_DESKTOP = "niri";
+# 		XDG_CURRENT_DESKTOP = "niri";
+# 
+# 		# Терминал и редакторы
+# 		TERMINAL = "kitty";
+# 		EDITOR = "micro";
+# 		SUDO_EDITOR = "micro";
+# 		VISUAL = "subl";
+# 
+# 		# Kitty
+# 		KITTY_ENABLE_WAYLAND = "1";
+# 
+# 		# GTK/ATK
+# 		NO_AT_BRIDGE = "1";
+# 
+# 		# NVIDIA Wayland поддержка
+# 		GBM_BACKEND = "nvidia-drm";
+# 		__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+# 
+# 		# GDK/Clutter
+# 		GDK_BACKEND = "wayland,x11,*";
+# 		CLUTTER_BACKEND = "wayland";
+# 		CLUTTER_DEFAULT_FPS = "60";
+# 
+# 		# Qt
+# 		QT_QPA_PLATFORM = "wayland;xcb";
+# 		QT_QPA_PLATFORMTHEME = "qt6ct";
+# 		QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
+# 
+# 		# SDL
+# 		SDL_VIDEODRIVER = "wayland,x11,windows";
+# 
+# 		# Java
+# 		_JAVA_AWT_WM_NONREPARENTING = "1";
+# 
+# 		# Electron
+# 		ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+# 
+# 		# NVIDIA кодеков
+# 		GST_PLUGIN_FEATURE_RANK = "nvmpegvideodec:MAX,nvmpeg2videodec:MAX,nvmpeg4videodec:MAX,nvh264sldec:MAX,nvh264dec:MAX,nvjpegdec:MAX,nvh265sldec:MAX,nvh265dec:MAX,nvvp9dec:MAX";
+# 		GST_VAAPI_ALL_DRIVERS = "1";
+# 
+# 		# VA-API/VDPAU
+# 		LIBVA_DRIVER_NAME = "nvidia";
+# 		VAAPI_MPEG4_ENABLED = "true";
+# 		VDPAU_DRIVER = "nvidia";
+# 
+# 		# Firefox
+# 		MOZ_DISABLE_RDD_SANDBOX = "1";
+# 		MOZ_ENABLE_WAYLAND = "1";
+# 		MOZ_X11_EGL = "1";
+# 
+# 		# NVIDIA Direct Rendering
+# 		NVD_BACKEND = "direct";
+# 
+# 		# OBS Studio
+# 		OBS_USE_EGL = "1";
+# 
+# 		# MangoHud
+# 		MANGOHUD = "1";
+# 		MANGOHUD_DLSYM = "1";
+# 
+# 		# Wine
+# 		# //WINEPREFIX = "$HOME/.wine";
+# 		# //WINEARCH = "win64";
+# 		STAGING_SHARED_MEMORY = "1";
+# 
+# 		# NVIDIA OpenGL оптимизации
+# 		__GL_SHADER_CACHE = "1";
+# 		__GL_SHADER_DISK_CACHE = "1";
+# 		__GL_SHADER_DISK_CACHE_SKIP_CLEANUP = "1";
+# 		__GL_ExperimentalPerfStrategy = "1";
+# 		__GL_ConformantBlitFramebufferScissor = "1";
+# 		__GL_MaxFramesAllowed = "1";
+# 		__GL_SYNC_TO_VBLANK = "0";
+# 		__GL_YIELD = "NOTHING";
+# 
+# 		# Ввод
+# 		GLFW_IM_MODULE = "none";
+# 
+# 		# Синхронизация/VSync
+# 		mesa_glthread = "true";
+# 		vblank_mode = "0";
+# 		gl_vsync = "0";
+# 		vsync = "1";  # Может конфликтовать с vblank_mode=0
+# 
+# 		# Vulkan
+# 		MESA_VK_WSI_PRESENT_MODE = "immediate";
+# 
+# 		# DXVK
+# 		DXVK_SHADER_OPTIMIZE = "1";
+# 		DXVK_ENABLE_NVAPI = "1";
+# 		DXVK_ASYNC = "1";
+# 		DXVK_FRAME_RATE = "60";
+# 		DXVK_CONFIG = "dxgi.syncInterval=0; d3d9.presentInterval=0";
+# 
+# 		# VkBasalt
+# 		ENABLE_VKBASALT = "0";
+# 
+# 		# Аудио
+# 		PIPEWIRE_LATENCY = "512/48000";
+# 		PULSE_LATENCY_MSEC = "60";
+# 
+# 		# Proton
+# 		PROTON_ENABLE_NGX_UPDATER = "1";
+# 		PROTON_ENABLE_NVAPI = "1";
+# 		PROTON_FORCE_LARGE_ADDRESS_AWARE = "1";
+# 		PROTON_HIDE_NVIDIA_GPU = "0";
+# 		PROTON_USE_NTSYNC = "1";
+# 		# //PROTON_ENABLE_WAYLAND = "1";
+# 		PROTON_LOG = "1";
+# 
+# 		# VKD3D
+# 		VKD3D_CONFIG = "dxr";
+# 
+# 		# Wayland/XWayland
+# 		vk_xwayland_wait_ready = "false";
+# 
+# 		# GTK настройки
+# 		GTK_USE_IEC_UNITS = "1";
+# 		GTK_OVERLAY_SCROLLING = "1";
+# 		GTK_USE_PORTAL = "1";
+# 		GDK_DEBUG = "portals";
+# 
+# 		# NixOS специфичные
+# 		NIXOS_OZONE_WL = "1";
+# 
+# 		# Telegram Desktop
+# 		# TDESKTOP_USE_GTK_FILE_DIALOG = "1";
+# 		# TDESKTOP_I_KNOW_ABOUT_GTK_INCOMPATIBILITY = "1";
 	};
 	
-	# services.xserver.enable = false;
 
 	system.stateVersion = "25.05";
 }
