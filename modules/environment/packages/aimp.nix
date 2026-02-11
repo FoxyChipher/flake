dd9528d99ff49354bb10297ed513179bde28caba
FoxyChipher <ageev-eldar@mail.ru>
2026-02-12 02:38:56 +0300

    aimp wip wip

--- modules/environment/packages/aimp.nix
+++ modules/environment/packages/aimp.nix
@@ -1,13 +1,22 @@
 { pkgs, ... }:
 
 let
-  buildInputs = with pkgs; [
-    gtk2 cairo pango atk gdk-pixbuf glib libpulseaudio alsa-lib libGL libGLU
-    fontconfig freetype dbus libsecret curl openssl ffmpeg
-    hicolor-icon-theme adwaita-icon-theme
+  runtimeDeps = with pkgs; [
+    gtk3 gtk2 cairo pango atk gdk-pixbuf glib
+    libpulseaudio alsa-lib
+    libGL libGLU
+    fontconfig freetype dbus libsecret
+    curl
+      curl.out
+      curl.dev
+    openssl ffmpeg harfbuzz zlib sqlite
+    stdenv.cc.cc.lib
   ] ++ (with pkgs.xorg; [
-    libX11 libXext libXrender libXtst libXScrnSaver libXdamage libXfixes libXi
+    libX11 libXext libXrender libXtst
+    libXScrnSaver libXdamage libXfixes libXi
   ]);
+
+  libraryPath = pkgs.lib.makeLibraryPath runtimeDeps;
 in
 
 pkgs.stdenv.mkDerivation rec {
@@ -16,7 +25,7 @@ pkgs.stdenv.mkDerivation rec {
 
   src = pkgs.fetchurl {
     url = "https://www.aimp.ru/files/windows/builds/aimp-nightly-x86_64.pkg.tar.zst";
-    hash = "sha256-ooYvVtOgbfs/DE3oQsdvshKE/jsdH63vbFLUxEhRWcw=";  # Your SRI hash
+    hash = "sha256-ooYvVtOgbfs/DE3oQsdvshKE/jsdH63vbFLUxEhRWcw=";
   };
 
   nativeBuildInputs = with pkgs; [
@@ -25,46 +34,39 @@ pkgs.stdenv.mkDerivation rec {
     zstd
   ];
 
+  buildInputs = runtimeDeps;
+
+  unpackPhase = ''
+    mkdir -p source
+    ${pkgs.zstd}/bin/zstd -d -c $src | tar xf - -C source
+  '';
+
+ autoPatchelfIgnoreMissingDeps = false;
+  
+  autoPatchelfSearchPath = runtimeDeps;
   dontConfigure = true;
   dontBuild = true;
 
-  unpackPhase = ''
-    mkdir unpack
-    cd unpack
-    ${pkgs.zstd}/bin/zstd -d -c $src | tar x
-  '';
-
   installPhase = ''
-    # Find the main app dir (usually /opt/aimp)
-    APPDIR=$(find . -type d -name "*aimp*" | head -1)
-    if [ -z "$APPDIR" ]; then
-      echo "Error: No aimp directory found!" >&2
-      exit 1
-    fi
-
-    mkdir -p $out/opt/aimp $out/bin $out/share
-
-    # Copy app files
-    cp -r "$APPDIR"/* $out/opt/aimp/ || true
-
-    # Copy any share/icons
-    cp -r ./{usr,opt}/share/* $out/share/ 2>/dev/null || true
-
-    # Find binary and wrap
-    BIN=$(find $out/opt/aimp -type f -executable -name "*aimp*" -print -quit)
-    if [ -n "$BIN" ]; then
-      makeWrapper "$BIN" $out/bin/aimp \
-        --prefix LD_LIBRARY_PATH : ${pkgs.lib.makeLibraryPath buildInputs} \
-        --prefix XDG_DATA_DIRS : "$out/share:${pkgs.gtk2}/share:${pkgs.hicolor-icon-theme}/share"
-    else
-      echo "Error: No aimp binary found!" >&2
-      exit 1
-    fi
+    mkdir -p $out
+    cp -r source/* $out/
+  
+    chmod +x $out/opt/aimp/AIMP
+  
+    # Копируем libcurl внутрь приложения
+    cp ${pkgs.curl.out}/lib/libcurl.so.4 $out/opt/aimp/
+  
+    makeWrapper "$out/opt/aimp/AIMP" $out/bin/aimp \
+      --set XDG_DATA_DIRS "$out/usr/share:${pkgs.gtk3}/share"
   '';
-
+	postFixup = ''
+	  patchelf \
+	    --add-rpath ${pkgs.lib.makeLibraryPath runtimeDeps} \
+	    $out/opt/aimp/AIMP
+	'';
   meta = with pkgs.lib; {
-    description = "AIMP music player (nightly Arch build)";
-    homepage = "https://www.aimp.ru";
+    description = "AIMP 6 nightly build (native Linux)";
+    homepage = "https://www.aimp.ru/";
     license = licenses.unfree;
     platforms = platforms.linux;
     mainProgram = "aimp";
