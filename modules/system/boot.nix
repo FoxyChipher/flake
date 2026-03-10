c9f7767ea3014c8576b1b91bf29495e69cb8f2af
FoxyChipher <ageev-eldar@mail.ru>
2026-03-10 09:53:09 +0300

    WiP

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,18 +1,23 @@
 { stdenv, config, pkgs, lib, inputs, vars, ... }:
-	let
-
-	in
 {
 	boot = {
 #	==========	KERNEL	==========
 		kernelPackages = pkgs.linuxPackages_xanmod_latest;
 #	==========	BOOTLOADER	==========
-		loader = if vars.bootLoader == "GRUB" then {
+		loader =
+		if vars.bootLoader == "GRUB" then {
 			grub.enable = true;
 			grub.device = config.fileSystems."/boot".device;
+			limine.enable = false;
 		} else if vars.bootLoader == "Limine" then {
 			limine.enable = true;
 			limine.biosDevice = config.fileSystems."/boot".device;
+			limine.enableEditor = true;
+			limine.extraConfig = ''
+			        remember_last_entry: yes
+			      '';
+			grub.enable = false;
 		} else throw "Unknown boot loader: ${vars.bootLoader}";
 	};
 }
+
